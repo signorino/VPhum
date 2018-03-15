ecf656ba8498fbe18e42ce7c41b82e9cd9f7557a
darren.chen <darren.chen@vipshop.com>
2018-03-15 17:26:06 +0800

    初始化

---
+++ src/main/java/com/vphum/service/frontend/controller/IndexController.java
@@ -0,0 +1,7 @@
+package com.vphum.service.frontend.controller;
+
+/**
+ * Created by darren.chen on 2018/3/14.
+ */
+public class IndexController {
+}
---
+++ src/main/java/com/vphum/service/frontend/repository/dao/testDao.java
@@ -0,0 +1,7 @@
+package com.vphum.service.frontend.repository.dao;
+
+/**
+ * Created by darren.chen on 2018/3/15.
+ */
+public class testDao {
+}
